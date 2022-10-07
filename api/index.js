2fcda122982b4364a7ad27fa9f89639a194c8560
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-10-07 15:35:58 +0200

    added routes backend

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
 dotenv.config();
 const app = express();
 const PORT = 8800;
@@ -18,13 +22,11 @@ const connect = async () => {
 mongoose.connection.on("disconnected", () => {
   console.log("mongoDB disconnected");
 });
-mongoose.connection.on("connected", () => {
-  console.log("mongoDB connected");
-});
 
-app.get("/", (req, res) => {
-  res.send("hello firt request");
-});
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
 app.listen(PORT, () => {
   connect();
