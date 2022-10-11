a08fc56274e4c5f8f9b2939ad57aec82215014e7
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-10-11 20:16:35 +0200

    added json webtoken

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
 const PORT = 8800;
@@ -22,7 +23,7 @@ const connect = async () => {
 mongoose.connection.on("disconnected", () => {
   console.log("mongoDB disconnected");
 });
-
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
